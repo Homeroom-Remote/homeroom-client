7f1688965d7386f299efc109271fb35acf47c9ec
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2022-02-28 16:47:44 +0200

    adds loading state, spinner

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import useTheme from "./stores/themeStore";
 import useUser from "./stores/userStore";
 import { onAuthStateChanged, logout } from "./api/auth";
 
 import Welcome from "./components/Welcome";
+import { BallTriangle } from "./components/Spinners";
 
 function App() {
   const { theme } = useTheme();
-  const { user, setUser, isLoggedIn } = useUser();
+  const { setUser, isLoggedIn } = useUser();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     onAuthStateChanged((authState) => {
+      setLoading(true);
       setUser(authState?.multiFactor?.user);
+      setTimeout(() => setLoading(false), 1000);
     });
   }, [setUser]);
 
   const handleLogout = () => {
-    console.log(user);
     logout(
       () => console.log("Logged out succesfully"),
       () => console.warn("error logging out")
     );
   };
 
-  console.log("logged in", isLoggedIn());
+  if (loading) {
+    return (
+      <div className={theme}>
+        <div className="flex h-screen w-full items-center justify-center">
+          <BallTriangle />
+        </div>
+      </div>
+    );
+  }
+
   if (isLoggedIn()) {
     return (
       <div className={theme}>
-        <div className="w-screen h-screen bg-red-200">
+        <div className="w-screen h-screen dark:bg-background-800 bg-background-100">
           <button onClick={handleLogout}>Log out</button>
         </div>
       </div>
