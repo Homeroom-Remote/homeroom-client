ca6158451781e241e5c616bc723de16aac83ff6a
Tzahi Elhadad <tsahi4332@gmail.com>
2022-05-25 07:56:19 +0300

    add more questions

--- src/components/Home/Info/Info.js
+++ src/components/Home/Info/Info.js
@@ -15,38 +15,78 @@ export default function Info() {
                     <p> We can recognize 4 hand gestures: </p>
                 </div>
                 <div className="flex flex-row items-center">
-                    <p>Like: </p><img src={raiseHandIcon} style={{ height: 35 }} />
-                    <p>Dislike: </p><img src={raiseHandIcon} style={{ height: 35 }} />
+                    <p>Like: </p><img src={likeIcon} style={{ height: 35 }} />
+                    <p>Dislike: </p><img src={dislikeIcon} style={{ height: 35 }} />
                     <p>Raise Hand: </p><img src={raiseHandIcon} style={{ height: 35 }} />
-                    <p>Fist: </p><img src={raiseHandIcon} style={{ height: 35 }} />
+                    <p>Fist: </p><img src={fistIcon} style={{ height: 35 }} />
                 </div>
             </div>
 
         },
         {
-            question: "How can I ask question ?",
+            question: "What we can do with our Face Recognition feature?",
             answer: <div className="flex flex-row items-center">
-                <p>Raise Hand: </p><img src={raiseHandIcon} style={{ height: 35 }} />
+                <p>Homeroom can learn the participants feelings about the meeting,
+                    and reflect the meeting manager the listening and concentration level
+                    and know whether they understand the content of the lesson or not</p>
+            </div>
+
+        },
+        {
+            question: "What is our questions queue ?",
+            answer: <div className="flex flex-row items-center">
+                <p>The questions queue belongs to the meeting manager, questions added
+                    to the questions queue by students that raising hand and removed by fist gesture.</p>
+                <p>QUEUE IMAGE WILL BE HERE</p>
             </div>
 
         },
         {
             question: "How can I ask question ?",
             answer: <div className="flex flex-row items-center">
-                <p>Raise Hand: </p><img src={raiseHandIcon} style={{ height: 35 }} />
+                <p>Just raise your hand </p><img src={raiseHandIcon} style={{ height: 35 }} />
             </div>
 
         },
         {
-            question: "How can I ask question ?",
-            answer: <div className="flex flex-row items-center">
-                <p>Raise Hand: </p><img src={raiseHandIcon} style={{ height: 35 }} />
+            question: "How can I delete my question ?",
+            answer: <div className="flex flex-row gap-4 items-center">
+                <p>Make fist gesture with one hand up </p><img src={fistIcon} style={{ height: 35 }} />
             </div>
         },
     ]
     const rowsManager = [
-        { question: "MANAGER whatsUpp", answer: "everythingallwrite" },
-        { question: "MANAGER tzahi hello", answer: "blah blah blah " }
+        {
+            question: "How can I check how much students concentrated in the meeting ?",
+            answer: <div className="flex flex-row gap-4 items-center">
+                <p>ask OWEN </p><img src={fistIcon} style={{ height: 35 }} />
+            </div>
+        },
+        {
+            question: "How can I see my participants feelings about the meeting ?",
+            answer: <div className="flex flex-row gap-4 items-center">
+                <p>ask OWEN he know</p><img src={fistIcon} style={{ height: 35 }} />
+            </div>
+        },
+        {
+            question: "What is Homeroom satistics tab ?",
+            answer: <div className="flex flex-row gap-4 items-center">
+                <p>It's feature that based on participants feelings, for each meeting we calculate
+                    score for the meeting manager. </p><img src={fistIcon} style={{ height: 35 }} />
+            </div>
+        },
+        {
+            question: "How Homeroom calculate the manager meeting score ?",
+            answer: <div className="flex flex-row gap-4 items-center">
+                <p>ask OWEN </p>
+            </div>
+        },
+        {
+            question: "How can I see my meetings scores ?",
+            answer: <div className="flex flex-row gap-4 items-center">
+                <p>In satistics tab. </p>
+            </div>
+        },
     ]
     return (
         <div className="overflow-auto h-full relative ">
