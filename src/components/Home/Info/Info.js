b9507fe6bc3040904964f24672abd848c6c3a4a5
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2022-06-05 14:24:56 +0300

    faq information & styles

--- src/components/Home/Info/Info.js
+++ src/components/Home/Info/Info.js
@@ -1,118 +1,44 @@
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import InfoRow from "./InfoRow";
-import fistIcon from "../../../utils/fist_icon.png"
-import likeIcon from "../../../utils/like_icon.png"
-import dislikeIcon from "../../../utils/dislike_icon.png"
-import raiseHandIcon from "../../../utils/raise_hand_icon.png"
+import Questions from "./questions";
 
 export default function Info() {
-    const rowsGenaral = [
-        {
-            question: "What we can do with our Hand Recognition feature ?",
-            answer: <div className="flex flex-row items-center gap-10 text-xl">
-                <div>
-                    <p> We can recognize 4 hand gestures: </p>
-                </div>
-                <div className="flex flex-row items-center text-xl gap-2">
-                    <p>Like: </p><img src={likeIcon} style={{ height: 30 }} />
-                    <p>Dislike: </p><img src={dislikeIcon} style={{ height: 30 }} />
-                    <p>Raise Hand: </p><img src={raiseHandIcon} style={{ height: 38 }} />
-                    <p>Fist: </p><img src={fistIcon} style={{ height: 40 }} />
-                </div>
-            </div>
+  return (
+    <div className="overflow-auto h-full relative ">
+      <section className="absolute">
+        <div className="container w-full px-6 py-10 mx-auto">
+          <h1 className="text-4xl font-semibold text-center text-gray-800 dark:text-white ">
+            Frequently asked questions
+          </h1>
+          <br />
+          <br />
+          <h2 className="text-2xl font-semibold text-left text-gray-800 dark:text-white">
+            {" "}
+            General{" "}
+          </h2>
+          <div className="mt-12 space-y-8">
+            {Questions.GeneralFAQ.map(({ question, answer }) => {
+              return (
+                <InfoRow question={question} answer={answer} key={question} />
+              );
+            })}
+          </div>
+          <br />
+          <br />
 
-        },
-        {
-            question: "What we can do with our Face Recognition feature ?",
-            answer: <div className="flex flex-row items-center text-xl">
-                <p>Homeroom can learn the participants feelings about the meeting,
-                    and reflect the meeting manager the listening and concentration level
-                    and know whether they understand the content of the lesson or not</p>
-            </div>
-
-        },
-        {
-            question: "What is our questions queue ?",
-            answer: <div className="flex flex-row items-center text-xl">
-                <p>The questions queue belongs to the meeting manager, questions added
-                    to the questions queue by students that raising hand and removed by fist gesture.</p>
-                <p>QUEUE IMAGE WILL BE HERE</p>
-            </div>
-
-        },
-        {
-            question: "How can I ask question ?",
-            answer: <div className="flex flex-row items-center text-xl">
-                <p>Just raise your hand </p><img src={raiseHandIcon} style={{ height: 35 }} />
-            </div>
-
-        },
-        {
-            question: "How can I delete my question ?",
-            answer: <div className="flex flex-row gap-4 items-center text-xl">
-                <p>Make fist gesture with one hand up </p><img src={fistIcon} style={{ height: 35 }} />
-            </div>
-        },
-    ]
-    const rowsManager = [
-        {
-            question: "How can I check how much students concentrated in the meeting ?",
-            answer: <div className="flex flex-row gap-4 items-center text-xl">
-                <p>ask OWEN </p><img src={fistIcon} style={{ height: 35 }} />
-            </div>
-        },
-        {
-            question: "How can I see my participants feelings about the meeting ?",
-            answer: <div className="flex flex-row gap-4 items-center text-xl">
-                <p>ask OWEN he know</p><img src={fistIcon} style={{ height: 35 }} />
-            </div>
-        },
-        {
-            question: "What is Homeroom statistics tab ?",
-            answer: <div className="flex flex-row gap-4 items-center text-xl">
-                <p>It's feature that based on participants feelings, for each meeting we calculate
-                    score for the meeting manager. </p><img src={fistIcon} style={{ height: 35 }} />
-            </div>
-        },
-        {
-            question: "How Homeroom calculate the manager meeting score ?",
-            answer: <div className="flex flex-row gap-4 items-center text-xl">
-                <p>ask OWEN </p>
-            </div>
-        },
-        {
-            question: "How can I see my meetings scores ?",
-            answer: <div className="flex flex-row gap-4 items-center text-xl">
-                <p>In satistics tab. </p>
-            </div>
-        },
-    ]
-    return (
-        <div className="overflow-auto h-full relative ">
-            <section className="absolute">
-                <div className="container w-full px-6 py-10 mx-auto">
-                    <h1 className="text-4xl font-semibold text-center text-gray-800 dark:text-white ">Frequently asked questions</h1><br /><br />
-                    <h2 className="text-2xl font-semibold text-left text-gray-800 dark:text-white"> General </h2>
-                    <div className="mt-12 space-y-8">
-                        {rowsGenaral.map(({ question, answer }) => {
-                            return <InfoRow question={question} answer={answer} key={question} />
-                        })}
-                    </div><br /><br />
-
-                    <h2 className="text-2xl font-semibold text-left text-gray-800 dark:text-white"> For Meeting Manager </h2>
-                    <div className="mt-12 space-y-8">
-                        {rowsManager.map(({ question, answer }) => {
-                            return <InfoRow question={question} answer={answer} key={question} />
-                        })}
-                    </div>
-                    <br /><br />
-                    <h2 className="text-3xl font-semibold text-center text-gray-800 dark:text-white"> --- Homeroom --- </h2>
-                    <br />
-                </div>
-            </section>
-            
-            
+          <h2 className="text-2xl font-semibold text-left text-gray-800 dark:text-white">
+            For Meeting Manager
+          </h2>
+          <div className="mt-12 space-y-8">
+            {Questions.OwnerFAQ.map(({ question, answer }) => {
+              return (
+                <InfoRow question={question} answer={answer} key={question} />
+              );
+            })}
+          </div>
         </div>
-    );
+        <br />
+      </section>
+    </div>
+  );
 }
